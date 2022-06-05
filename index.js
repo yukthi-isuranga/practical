e2357969e89cac9bd429cb0f9b4d1d69a1780e1b
yukthi-isuranga <yukthiisuranga111@gmail.com>
2022-06-05 21:54:43 +0530

    my made

--- index.js
+++ index.js
@@ -14,8 +14,27 @@ admin.initializeApp({
 //   });
 // };
 
+// const db = admin.firestore();
+// let studentRef = db.collection("student");
+// studentRef.get().then((querySnapshot) => {
+//   querySnapshot.forEach(snapshot => console.log(snapshot.data()));
+// });
+
 const db = admin.firestore();
-let studentRef = db.collection("student");
-studentRef.get().then((querySnapshot) => {
+// const docRef = db.collection('employee');
+for (let index = 0; index < 5; index++) {
+  const docRef = db.collection("employee").doc(index.toString());
+
+  docRef.set({
+    firstName: index,
+    lastName: index,
+    born: index,
+    age: index,
+    gender: index,
+  });
+}
+
+const docRef = db.collection('employee');
+docRef.get().then((querySnapshot) => {
   querySnapshot.forEach(snapshot => console.log(snapshot.data()));
 });
